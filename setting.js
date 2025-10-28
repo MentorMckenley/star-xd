41ba2abfedda869d926aacaace458aa00e2c347c
MentorMckenley <81643980+MentorMckenley@users.noreply.github.com>
2025-10-28 18:34:38 -0400

    Update setting.js

--- setting.js
+++ setting.js
@@ -3,7 +3,7 @@
 module.exports = {
   BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
   OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263776388689', // add your phone number
+  OWNER_NUMBER: '50949181575', // add your phone number
   SESSION_ID: '', // use sessin id if inbuilt pairing not working
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare', // ur timezone
